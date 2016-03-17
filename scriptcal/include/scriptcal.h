9dd6bcce4cadd6134f38ead761a37fe2bbed1914
D000ben000 <764770837@qq.com>
2016-03-17 09:58:45 +0800

    20160317_cwj_merge

--- scriptcal/include/scriptcal.h
+++ scriptcal/include/scriptcal.h
@@ -4,17 +4,6 @@
 
 namespace poac
 {
-//	string Cal(string str);
-	//string Cal(string str)
-	//{
-	//	if (str.size() == 0)
-	//		return "it is null";
-	//	else
-	//		return "it is ok";
-	//}
-
-	int Foo(int x, int y){return x+y;}
-
 	class CNode{
 	public:
 		CNode();
--- scriptcal/test/test.cpp
+++ scriptcal/test/test.cpp
@@ -10,22 +10,9 @@ using namespace poac;
 
 TEST(FooTest, HandleNoneZeroInput)
 {
- //   EXPECT_EQ("it is null", Cal(NULL));
-//    EXPECT_EQ("it is ok", Cal("fdd"));
-
 	//EXPECT_EQ(3, Foo(2,1));
 	//EXPECT_EQ(4, Foo(2,2));
 	//EXPECT_EQ(8, Foo(5,3));
-
-	ifstream in("123.txt");
-	if (! in.is_open())
-		cout << "Error open" << endl;
-	while (!in.eof() )  
-    {  
-		int a, b, c;
-		in >> a >> b >> c;
-		EXPECT_EQ(a, Foo(b,c));
-    } 
 }
 
 
