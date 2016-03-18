3162f516e8e841340719021a3707a348adcb23a8
D000ben000 <764770837@qq.com>
2016-03-18 10:29:28 +0800

    201603181029_cwj

--- scriptcal/include/script.h
+++ scriptcal/include/script.h
@@ -1,12 +1,26 @@
 #ifndef _SCRIPT_H_
 #define _SCRIPT_H_
 
+#include <string>
+#include <iostream>
+using namespace std;
+
 namespace script{
+
+template<typename T>
+struct SVariableValue{
+	string m_Variable;
+	T m_Value;
+	SVariableValue(string name):m_Variable(name),m_Value(0){}
+};
 
 class CSript{
 public:
 	CSript();
 	bool InitScript(string input);
+
+private:
+
 };
 
 };
---
+++ scriptcal/src/script.cpp
@@ -0,0 +1,7 @@
+#include "script.h"
+
+using namespace script;
+
+bool CSript::InitScript(string input){
+	return true;
+}
