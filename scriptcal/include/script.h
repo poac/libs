905e32f13a4e7028dbeabdc83c908a550ff0a3a7
D000ben000 <764770837@qq.com>
2016-03-25 00:01:33 +0800

    20160324_cwj

--- scriptcal/include/script.h
+++ scriptcal/include/script.h
@@ -2,25 +2,33 @@
 #define _SCRIPT_H_
 
 #include <string>
+#include <vector>
 #include <iostream>
+#include "oprator.h"
 using namespace std;
+using namespace oprator;
 
 namespace script{
 
-template<typename T>
-struct SVariableValue{
-	string m_Variable;
-	T m_Value;
-	SVariableValue(string name):m_Variable(name),m_Value(0){}
-};
 
 class CSript{
 public:
 	CSript();
+	~CSript();
 	bool InitScript(string input);
 
 private:
-
+	// 对字符input判定输入类型
+	int GetTypeChar(char input);
+private:
+	enum ETypeChar{ 
+		_Unknow = -1,	// 未知类型
+		_Blank = 0,		// 空格，tab
+		_Operate = 1,	// 运算符
+		_Letter = 2,	// 字母
+		_Number = 3		// 数字+‘.’
+	};
+	COperationFactory m_OperFac;
 };
 
 };
--- scriptcal/src/script.cpp
+++ scriptcal/src/script.cpp
@@ -2,6 +2,58 @@
 
 using namespace script;
 
+// Script初始化工作
 bool CSript::InitScript(string input){
+	int len_input = input.size();
+	vector<string> part_Script;
+
+	int i_start = 0, len_varia = 1;	
+	while(i_start + len_varia -1 < len_input){
+		
+		char cNowPos = input[i_start+len_varia-1];
+		int iTypeNowPos = GetTypeChar(cNowPos);
+		if (iTypeNowPos == 0 || iTypeNowPos == 1)
+		{// 出现空格\tab\运算符
+			string temp_var = input.assign(input, i_start, len_varia);
+			// 若temp_var为空格或tab
+			if (len_varia == 1 && iTypeNowPos == _Blank) {
+				i_start++;
+				len_varia = 1;
+			}
+			else if (len_varia == 1 && iTypeNowPos == _Operate) {
+
+			}
+		}
+		else{// 字符
+			len_varia++;
+		}
+
+
+	}
+
 	return true;
 }
+
+
+// 对字符input判定输入类型：
+int CSript::GetTypeChar(char input){
+	if (input == ' ' || input == '	') {
+		return _Blank;
+	}
+
+	if (m_OperFac.isOperation(input)) {
+		return _Operate;
+	}
+
+	if ((input <= 'z' && input >= 'a') || (input <= 'Z' && input >= 'A'))
+	{
+		return _Letter;
+	}
+
+	if ((input <= '9' && input >= '0') || input == '.')
+	{
+		return _Number;
+	}
+
+	return _Unknow;
+}//GetTypeChar
