eb94a316d1e1f4a0fc5deddbaf13b4f0a0dfb5da
D000ben000 <764770837@qq.com>
2016-03-28 16:01:54 +0800

    20160328_cwj_格式调制，命名空间

--- scriptcal/include/script.h
+++ scriptcal/include/script.h
@@ -6,37 +6,32 @@
 #include <iostream>
 #include "oprator.h"
 using namespace std;
-using namespace oprator;
 
-namespace poac
-{ namespace script{
-
-
-
-class CScript{
-public:
-	CScript(){}
-	~CScript(){}
-	bool InitScript(string input);
-	void PrintScript();
-private:
-	// 对字符input判定输入类型
-	int GetTypeChar(char input);
-	// 对输入的字符串分词
-	bool SeparateStr(string input);
-private:
-	enum ETypeChar{ 
-		_Unknow = -1,	// 未知类型
-		_Blank = 0,		// 空格，tab,';'
-		_Operate = 1,	// 运算符
-		_Letter = 2,	// 字母
-		_Number = 3		// 数字+‘.’
+namespace poac{ 
+	namespace script{
+		class CScript{
+		public:
+			CScript(){}
+			~CScript(){}
+			bool InitScript(string input);
+			void PrintScript();
+		private:
+			// 对字符input判定输入类型
+			int GetTypeChar(char input);
+			// 对输入的字符串分词
+			bool SeparateStr(string input);
+		private:
+			enum ETypeChar{ 
+				_Unknow = -1,	// 未知类型
+				_Blank = 0,		// 空格，tab,';'
+				_Operate = 1,	// 运算符
+				_Letter = 2,	// 字母
+				_Number = 3		// 数字+‘.’
+			};
+			COperationFactory m_OperFac;	// 操作符工厂
+			vector<string> m_ScriptStrPart;
+		};
 	};
-	COperationFactory m_OperFac;	// 操作符工厂
-	vector<string> m_ScriptStrPart;
-};
-
-};
 };
 
 #endif
